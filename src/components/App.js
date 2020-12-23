2dbb3d77158609db58bb5308a16c6d38ee5f8c33
SMakaiTakori <makait87@icloud.com>
2020-12-22 22:42:18 -0500

    set up state and initial seed data

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const App = () => {
-  return <div></div>;
+const Todo = ({ todo, index }) => {
+  return <div className="todo">{todo.text}</div>;
+};
+
+const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      text: "Write Blog Post ",
+      isCompleted: false,
+    },
+    {
+      text: "Buy Krystle's christmas gift",
+      isCompleted: false,
+    },
+    {
+      text: "Update Resume",
+      isCompleted: false,
+    },
+  ]);
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default App;
