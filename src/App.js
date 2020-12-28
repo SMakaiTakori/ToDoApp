de72c4475498e6323f9188ba561345d568bac8cb
SMakaiTakori <makait87@icloud.com>
2020-12-27 22:08:31 -0500

    added todo input form, toggle for className/styling update on todo items

--- src/App.js
+++ src/App.js
@@ -2,24 +2,69 @@ import React, { useState } from "react";
 import "./App.css";
 
 const Todo = ({ todo, index }) => {
-  return <div className="todo">{todo.text}</div>;
+  const [isActive, setActive] = useState("false");
+
+  const handleToggle = () => {
+    setActive(!isActive);
+  };
+
+  return (
+    <>
+      <li className={isActive ? "todo" : "is-done"} onClick={handleToggle}>
+        {todo.item}
+      </li>
+
+      <style></style>
+    </>
+  );
+};
+
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <>
+      <form>
+        <input
+          type="text"
+          className="input"
+          placeholder="Add Todo Item..."
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </form>
+      <button onClick={handleSubmit}> Add Todo </button>
+    </>
+  );
 };
 
 const App = () => {
   const [todos, setTodos] = useState([
     {
-      text: "Write Blog Post ",
+      item: "Write Blog Post ",
       isCompleted: false,
     },
     {
-      text: "Buy Krystle's christmas gift",
+      item: "Buy Krystle's christmas gift",
       isCompleted: false,
     },
     {
-      text: "Update Resume",
+      item: "Update Resume",
       isCompleted: false,
     },
   ]);
+
+  const addTodo = (item) => {
+    const newTodos = [...todos, { item }];
+    setTodos(newTodos);
+  };
 
   return (
     <div className="app">
@@ -27,9 +72,24 @@ const App = () => {
         {todos.map((todo, index) => (
           <Todo key={index} index={index} todo={todo} />
         ))}
+        <TodoForm addTodo={addTodo} />
       </div>
     </div>
   );
 };
+
+// function solution(S) {
+//   // write your code in JavaScript (Node.js 8.9.4)
+//   const charMap = {};
+
+//   S.split("").forEach((char) => {
+//     if (charMap[char]) charMap[char]++;
+//     else charMap[char] = 1;
+//   });
+
+//   for (let char in charMap) {
+//     if (charMap[char] === 2) return char;
+//   }
+// }
 
 export default App;
